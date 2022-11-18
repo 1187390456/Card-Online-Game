4f556e5223f98178bdfd21cace58fc02fad1d013
XuChenMing <1187390456@qq.com>
2022-11-18 15:00:46 +0800

    完善计时器动画

--- Assets/Scripts/UI/Fight/TableCavansArea.cs
+++ Assets/Scripts/UI/Fight/TableCavansArea.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 using UnityEngine.UI;
+using System.Linq.Expressions;
 
 public class TableCavansArea : UIBase
 {
+    public List<Sprite> imageList = new List<Sprite>(); // 0-9 的图片资源 非动态载入 不清除
+
     private GameObject MatchTips; // 匹配提示
     private GameObject StartBtn; // 开始按钮
 
     private Button mingPaiStart; // 明牌开始
     private Button defaultStart; // 默认开始
+    private Button cancleMatch; // 取消匹配
 
     private Image shiWei; // 匹配时间十位
     private Image geWei;  // 匹配时间个位
@@ -18,17 +22,19 @@ public class TableCavansArea : UIBase
 
     private Queue<RectTransform> rectQueue = new Queue<RectTransform>(); // 动画 rectTrans队列
     private int sequenceIndex = 1; // 系列动画索引
-    public List<Sprite> imageList = new List<Sprite>(); // 0-9 的图片资源
 
     private void Awake()
     {
         StartBtn = transform.Find("StartBtn").gameObject;
+
         mingPaiStart = StartBtn.transform.Find("MingPaiStart").GetComponent<Button>();
         defaultStart = StartBtn.transform.Find("DefaultStart").GetComponent<Button>();
         mingPaiStart.onClick.AddListener(OnClickMingPaiStart);
         defaultStart.onClick.AddListener(OnClickDefaultStart);
 
         MatchTips = transform.Find("MatchTips").gameObject;
+        cancleMatch = MatchTips.transform.Find("Cancle").GetComponent<Button>();
+        cancleMatch.onClick.AddListener(OnClickCancleMatch);
 
         shiWei = MatchTips.transform.Find("Timer/ShiWei").GetComponent<Image>();
         geWei = MatchTips.transform.Find("Timer/Gewei").GetComponent<Image>();
@@ -42,8 +48,11 @@ public class TableCavansArea : UIBase
     public override void OnDestroy()
     {
         base.OnDestroy();
-        CancelInvoke(nameof(AnimationForVertical));
-        rectQueue.Clear();
+
+        RemoveAllBtnListen();
+        CancleAllInvoke();
+        ClearAllCollision();
+
         DOTween.KillAll();
     }
 
@@ -54,8 +63,8 @@ public class TableCavansArea : UIBase
     {
         SetMathchTipsActive(true);
         SetStartBtnActive(false);
-        InitAllAnimation();
-        StartTimer();
+
+        StartAnimation();
     }
 
     /// <summary>
@@ -65,16 +74,40 @@ public class TableCavansArea : UIBase
     {
         SetMathchTipsActive(true);
         SetStartBtnActive(false);
-        InitAllAnimation();
-        StartTimer();
+
+        StartAnimation();
     }
 
     /// <summary>
-    /// 初始入队
+    /// 取消匹配
+    /// </summary>
+    private void OnClickCancleMatch()
+    {
+        SetMathchTipsActive(false);
+        SetStartBtnActive(true);
+
+        ResetAnimaton();
+    }
+
+    /// <summary>
+    /// 重置动画
+    /// </summary>
+    private void ResetAnimaton()
+    {
+        CancleAllInvoke();
+        ClearAllCollision();
+        startTime = 30;
+        sequenceIndex = 1;
+        DOTween.KillAll();
+    }
+
+    /// <summary>
+    /// 开始动画
     /// </summary>
 
-    private void InitAllAnimation()
+    private void StartAnimation()
     {
+        StartTimer();
         var parent = MatchTips.transform.Find("AnimationTips");
         for (var i = 0; i < parent.childCount; i++)
         {
@@ -117,13 +150,84 @@ public class TableCavansArea : UIBase
     {
         Sequence sequence = DOTween.Sequence();
         var startPos = rectTrans.anchoredPosition;
-        var endPos = new Vector2(rectTrans.anchoredPosition.x, rectTrans.anchoredPosition.y + 10.0f);
+        var endPos = new Vector2(rectTrans.anchoredPosition.x, rectTrans.anchoredPosition.y + 12.0f);
         var t1 = DOTween.To(() => rectTrans.anchoredPosition, x => rectTrans.anchoredPosition = x, endPos, .2f);
         var t2 = DOTween.To(() => rectTrans.anchoredPosition, x => rectTrans.anchoredPosition = x, startPos, .2f);
         sequence
             .Append(t1)
             .Append(t2);
         sequence.SetEase(Ease.Flash);
+        sequence.onKill = () =>
+        {
+            rectTrans.anchoredPosition = startPos;
+        }; // 被杀了就重置位置
+    }
+
+    /// <summary>
+    /// 开始计时
+    /// </summary>
+    private void StartTimer()
+    {
+        if (startTime <= 0) startTime = 30;
+        SwitchCase(startTime % 10, geWei);  // 个位
+        SwitchCase((startTime % 100) / 10, shiWei); // 十位
+        startTime -= 1;
+        Invoke(nameof(StartTimer), 1.0f);
+    }
+
+    /// <summary>
+    /// 计时动画条件
+    /// </summary>
+    /// <param name="condition"></param>
+    /// <param name="targerImage"></param>
+    private void SwitchCase(int condition, Image targerImage)
+    {
+        switch (condition)
+        {
+            case 0:
+                targerImage.overrideSprite = imageList[0];
+                break;
+
+            case 1:
+                targerImage.overrideSprite = imageList[1];
+                break;
+
+            case 2:
+                targerImage.overrideSprite = imageList[2];
+                break;
+
+            case 3:
+                targerImage.overrideSprite = imageList[3];
+                break;
+
+            case 4:
+                targerImage.overrideSprite = imageList[4];
+                break;
+
+            case 5:
+                targerImage.overrideSprite = imageList[5];
+                break;
+
+            case 6:
+                targerImage.overrideSprite = imageList[6];
+                break;
+
+            case 7:
+                targerImage.overrideSprite = imageList[7];
+                break;
+
+            case 8:
+                targerImage.overrideSprite = imageList[8];
+                break;
+
+            case 9:
+                targerImage.overrideSprite = imageList[9];
+                break;
+
+            default:
+                break;
+        }
+        targerImage.SetNativeSize();
     }
 
     /// <summary>
@@ -139,46 +243,29 @@ public class TableCavansArea : UIBase
     private void SetStartBtnActive(bool value) => StartBtn.SetActive(value);
 
     /// <summary>
-    /// 开始计时
+    /// 移除所有按钮监听
     /// </summary>
-    private void StartTimer()
+    private void RemoveAllBtnListen()
     {
-        switch (startTime % 10)
-        {
-            case 0:
-                break;
+        mingPaiStart.onClick.RemoveAllListeners();
+        defaultStart.onClick.RemoveAllListeners();
+        cancleMatch.onClick.RemoveAllListeners();
+    }
 
-            case 1:
-                break;
+    /// <summary>
+    /// 取消所有异步事件
+    /// </summary>
+    private void CancleAllInvoke()
+    {
+        CancelInvoke(nameof(AnimationForVertical));
+        CancelInvoke(nameof(StartTimer));
+    }
 
-            case 2:
-                break;
-
-            case 3:
-                break;
-
-            case 4:
-                break;
-
-            case 5:
-                break;
-
-            case 6:
-                break;
-
-            case 7:
-                break;
-
-            case 8:
-                break;
-
-            case 9:
-                break;
-
-            default:
-                break;
-        }
-        startTime -= 1;
-        Invoke(nameof(StartTimer), 1.0f);
+    /// <summary>
+    /// 清除所有集合
+    /// </summary>
+    private void ClearAllCollision()
+    {
+        rectQueue.Clear();
     }
 }
