467d7a95a523c71c41e965e8283d2430699f6e74
xcm <1187390456@qq.com>
2022-11-25 22:02:05 +0800

    工具文件

---
+++ Assets/Scripts/Tools/Single/CloseMask.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CloseMask : MonoBehaviour, IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        transform.parent.gameObject.SetActive(false);
+    }
+}
