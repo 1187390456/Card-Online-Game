6a04f04130f9e36efcb80231705ab75a10bc73ce
XuChenMing <1187390456@qq.com>
2023-02-27 19:10:28 +0800

    服务器端配置

--- Assets/Scripts/Scene/MainScene.cs
+++ Assets/Scripts/Scene/MainScene.cs
@@ -7,7 +7,7 @@ public class MainScene : UIBase
     private void Start()
     {
         //TODO 测试用
-        DispatchTools.Load_Scence(Dispatch, 2);
+        // DispatchTools.Load_Scence(Dispatch, 2);
         Dispatch(AreaCode.AUDIO, AudioEvent.Play_Music_Audio, BackGroundMuscicType.Welcome.ToString()); // 播放背景音乐
     }
 }
